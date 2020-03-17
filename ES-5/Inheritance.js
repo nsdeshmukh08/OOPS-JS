1f9535e4c9bcfedc515d23664e704f54f60836b9
nsdeshmukh08 <nsdeshmukh08.com>
2020-03-17 22:29:01 +0530

    ES 5 Concepts

---
+++ ES-5/Inheritance.js
@@ -0,0 +1,42 @@
+function Employee(name, dept, salary){
+    this.dept = dept;
+    this.name = name;
+    this.salary = salary;
+}
+
+Employee.prototype.getName = function(){
+    return this.name;
+}
+
+Employee.prototype.getDepartment = function(){
+    return this.dept;
+}
+
+Employee.prototype.getSalary = function(){
+    return this.salary;
+}
+
+// Manager is a type of employee and hence it will inherit the Employee class.
+function Manager(name, dept, salary){
+    this.dept = dept;
+    this.name = name;
+    this.salary = salary;
+}
+
+Manager.prototype = new Employee(this.name, this.dept);
+
+// Method overloading doesn't support in JavaScript. 
+Manager.prototype.getSalary = function(salary){
+    return salary;
+}
+// If method have same name it will overwrites the previous method.
+Manager.prototype.getSalary = function(){
+    return this.salary;
+}
+
+var mgr1 = new Manager('Narayan', 'IT', '1lakh');
+
+console.log("===>", mgr1.getName());
+console.log("===>", mgr1.getDepartment());
+// This function will get call from the manager's prototype.
+console.log("===>", mgr1.getSalary(123));
